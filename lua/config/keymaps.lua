3c69fd8ebb36c943585865c96918ccc0a52be7d3
Marcos Daniel Nottingham <marcos.nottingham@detran.ce.gov.br>
2024-03-05 09:15:48 -0300

    add vim-rails vim-test bundler mini-comment and keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,7 @@ keymap.set("n", "<C-a>", "gg<S-v>G")
 -- Split window
 keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+keymap.set("n", "<leader>tt", ":TestNearest<CR>", opts)
+keymap.set("n", "<leader>tf", ":TestFile<CR>", opts)
+keymap.set("n", "<leader>ta", ":TestSuite<CR>", opts)
